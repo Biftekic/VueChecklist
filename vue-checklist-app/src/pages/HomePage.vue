c94426f3ff9fb9c9113182257dac5d31ddc57f04
Biftekic <tinpavlic@gmail.com>
2025-08-17 22:45:59 +0000

    test: Add MainLayout wrapper to test

--- vue-checklist-app/src/pages/HomePage.vue
+++ vue-checklist-app/src/pages/HomePage.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-container>
-    <h1>Test Home Page</h1>
-    <p>If you see this, the app is working!</p>
-    
-    <v-btn color="primary" @click="navigateTo('/checklists')">
-      Go to Checklists
-    </v-btn>
-  </v-container>
+  <MainLayout title="Cleaning Checklist Pro">
+    <v-container class="pa-4">
+      <h1>Test Home Page with MainLayout</h1>
+      <p>If you see this, MainLayout is working!</p>
+      
+      <v-btn color="primary" @click="navigateTo('/checklists')">
+        Go to Checklists
+      </v-btn>
+    </v-container>
+  </MainLayout>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router'
+import MainLayout from '@/layouts/MainLayout.vue'
 
-console.log('HomePage.vue loaded - test version')
+console.log('HomePage.vue loaded - with MainLayout')
 
 const router = useRouter()
 
